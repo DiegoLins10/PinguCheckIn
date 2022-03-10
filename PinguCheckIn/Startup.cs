81343f9aee812723d5dbfa9f4fb64fd78f31fb55
diegolins10 <dede140126>
2022-03-10 20:40:22 -0300

    initial

---
+++ PinguCheckIn/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using PinguCheckIn.Data;
+using Microsoft.OpenApi.Models;
+
+namespace PinguCheckIn
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<PinguCheckInContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PinguCheckInContext")));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Pingu API",
+                        Version = "v1",
+                        Description = "APIs de desenvolvimento do projeto Pingu CheckIn onde será uma aplicação de reservas de quartos de hotel utilizando ASP.NET Core e Entity Framework Core",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Diego Lins",
+                            Url = new System.Uri("//github.com/DiegoLins10/ECommerceSite")
+                        }
+                    });
+            });
+        }
+    
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pingu CheckIn v1"));
+        }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
