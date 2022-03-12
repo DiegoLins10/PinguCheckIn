97ce53441b44c50135ec6d7411b9c7b4687c1a94
diegolins10 <dede140126>
2022-03-11 23:33:43 -0300

    tela de login

--- PinguFront/src/app/app-routing.module.ts
+++ PinguFront/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'foo', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
