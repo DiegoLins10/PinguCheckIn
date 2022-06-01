8decb4d524ae364f57f90d22d5a649e34ae95c27
diegolins10 <dede140126>
2022-06-01 18:44:31 -0300

    ok

---
+++ PinguCheckIn/Models/Dtos/AdministradorControleDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PinguCheckIn.Models.Dtos
+{
+    public class AdministradorControleDto
+    {
+        public int IdReserva { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Quarto { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set; }
+        public int Status { get; set; }
+    }
+}
